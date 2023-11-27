b79d92e30ca9efc151f70dc9dd09b0b39d405d4e
Chris Pinto <Chris.pinto1234@gmail.com>
2023-11-27 12:55:10 -0330

    setting it all up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+// src/App.js
+import React from 'react';
+import Home from './components/Home';
 
 function App() {
-  
+  return (
+    <div>
+      <header>
+        {/* Your header component goes here */}
+      </header>
+      <main>
+        <Home />
+      </main>
+      <footer>
+        {/* Your footer component goes here */}
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+// src/components/Home.js
+import React from 'react';
+import '../styles/Home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Daily Stock News</h1>
+      <p>Stay informed with the latest stock news personalized just for you!</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default Home;
